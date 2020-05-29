b496b9703385e8645e9544eae219cc46df1f8c81
my vu <myxvux@gmail.com>
2020-05-29 13:51:02 +0200

    add function do_nothing to main.py (causes error in heroku but not locally)

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from document_classification.document_analyzer import document_analyzer
-import document_classification
-from document_classification.model_trainer import do_nothing
 app = Flask(__name__)
 app.register_blueprint(document_analyzer, url_prefix='/analyzer')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
+
 @app.route('/')
 def index():
     return "<h1>document classification</h1>"
+
+
+def do_nothing(tokens):
+    return tokens
+
